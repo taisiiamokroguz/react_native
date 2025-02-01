6ca7ebf4387a0dc599f4fa0df1b38461cb12a301
Taisiia <taya13taya@gmail.com>
2025-02-01 19:22:58 +0200

    BbcNewc cards

--- App.js
+++ App.js
@@ -1,28 +1,32 @@
 
 import { View,Text, Button, StyleSheet, Pressable } from "react-native";
-import ProductCard from "./components/ProductCard";
+import BbcComponent from "./components/BbcComponent";
 
 export default function App() {
   const styles = StyleSheet.create({
-    my_view: {
+    main_view:{
       flex: 1, 
-      justifyContent: "center",
-      alignItems: "center",
-      padding: 20,
-      gap:20,
-      width:"100%",
-      height:"auto",
-      backgroundColor:"lightblue"
-
-    },
-    
-  })
-  return(
-    <View style={styles.my_view}>
-      <ProductCard photoUri="https://ronin.in.ua/content/images/12/600x600l80mc0/33196887128631.webp" title="Миша безпровідна Bluetooth або USB Logitech M350 Pebble Wireless Sand"></ProductCard>
-     
+      padding:40,
+      justifyContent:"center",
+      gap:20
+    }
+  });
+  return (
+    <View style={styles.main_view}>
+      <BbcComponent photoUri="https://cdn.pixabay.com/photo/2023/08/05/16/25/elephants-8171393_640.jpg" 
+      title="Chikankari: A Lucknawi Tradition"
+      date="January 31, 2025"
+      category="USA" />
+       <BbcComponent photoUri="https://www.fairobserver.com/wp-content/uploads/2025/01/The-Gaza-Ceasefire-Deal.jpg" 
+      title="The Gaza Ceasefire Deal: A False White Flag or Path to Peace?"
+      date="January 30, 2025"
+      category="POLITICS" />
+       <BbcComponent photoUri="https://www.fairobserver.com/wp-content/uploads/2025/01/Indias-Strategic-Accomplishments-in-2024Ceasefire-Deal.jpg" 
+      title="A Year in Review: India’s Strategic Accomplishments in 2024"
+      date="February 1, 2025"
+      category="INDIA NEWS" />
+      
     </View>
   );
 }
-
 
--- components/BbcComponent.js
+++ components/BbcComponent.js
@@ -1,10 +1,49 @@
-import {View, Text, Image} from "react-native"
-export default function BbcComponent({photoUri = "", title="Title", date = "21 Jan 2024", country="Ukraine" }){
+import {View, Text, Image, StyleSheet} from "react-native"
+export default function BbcComponent({photoUri = "", title="Title", date = "21 Jan 2024", category="Ukraine" }){
+     const styles = StyleSheet.create({
+        n_view:{ 
+          padding:10,
+          borderWidth:1,
+          borderColor:"grey",
+          width:300, 
+          height:"auto", 
+          flexDirection:"row",
+          alignItems: "flex-start"
+        },
+        n_photo:{
+            width: 100, 
+            height: 100
+        },
+        inner_view:{
+            flexDirection:"column",
+            flex: 1,
+            justifyContent: "center",
+            marginLeft: 10 
+        },
+        n_title:{ 
+            fontWeight:"bold",
+            fontSize:16,
+            width: "100%", 
+            flexShrink: 1,
+            flexWrap: "wrap",
+            overflow: "hidden"
+        },
+        n_info:{
+            color:"#a0a0a0",
+            width: "100%", 
+            flexShrink: 1,
+            flexWrap: "wrap",
+            overflow: "hidden",
+            marginTop: "auto"
+
+        }
+      });
  return(
-    <View style={{width:"45%", height:"15%", flexDirection:"row"}}>
-        <Image source={{uri:photoUri}} style={{ width: '100%', height: '100%' }}></Image>
-        <View style={{flexDirection:"column"}}>
-        <Text style={{marginLeft:15, fontWeight:"bold"}}>{title}</Text>
+    <View style={styles.n_view}>
+        <Image source={{uri:photoUri}} style={styles.n_photo}></Image>
+        <View style={styles.inner_view}>
+        <Text style={styles.n_title}>{title}</Text>
+        <Text style={styles.n_info}>{date} | {category}</Text>
         </View>
     </View>
  );
